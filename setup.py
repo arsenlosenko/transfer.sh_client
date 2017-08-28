21e148ca471907a47bde5a92c0499f0bc13cef28
Arsen Losenko <arsenlosenko@gmail.com>
2017-08-28 22:45:45 +0300

    Created a valid setup.py file for the project

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-current_dir = path.abspath(path.dirname(__file__))
-
-with open(path.join(current_dir, 'README.md'), encoding="utf-8") as f:
-    description = f.read()
+from os.path import join, dirname
 
 setup(
-    name="transfersh_uploader",
-    version='0.0.1',
-    description="transfer.sh command line client",
-    url="https://github.com/arsenlosenko/transfer.sh-uploader",
-    author="Arsen Losenko",
-    author_email="arsenlosenko@gmail.com",
-    license="MIT",
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4'
-    ],
-
-    keywords='transfer.sh client uploader CLI',
-    py_modules=['transfer_file'],
-    entry_points={
-            'console_scripts':[
-                'transfer_files=transfer_file',
-            ],
-        },
+        name="transfersh_client",
+        version="1.0",
+        author="Arsen Losenko",
+        author_email="arsenlosenko@gmail.com",
+        description="transfer.sh command-line client",
+        url="https://github.com/arsenlosenko/transfer.sh_client.git",
+        keywords="transfer.sh command-line client tool utility cli CLI",
+        license="MIT",
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities'
+            'Environment :: Console',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.4'
+        ],
+        packages=find_packages(),
+        long_description=open(join(dirname(__file__), "README.md")).read(),
+        install_requires=[
+            'requests==2.2.1',
+            'pyperclip==1.5.27',
+        ],
+        entry_points={
+                "console_scripts":
+                ['transfer_files = client.app:main'],
+            },
     )
 
 
